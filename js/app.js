018d2fc4c6421cce4a6c8a88b07867341b3d6064
kooliotbone <kooliotbone@gmail.com>
2024-10-31 10:57:35 -0700

    New Features
    added additional features to the function "makeActive". It will now also assign class "active-button" to the navigation bar buttons as the user scrolls through the sections to show which section is the user on currently.
    
    Added "hideNavBar" function to make sure the navigation bar is hidden if the user is not scrolling.
    
    Added the "scrollAction" function to provide smoother scrolling to the specific section when clicked.
    
    Added a "Return to Top" button at the bottom of the page right above the footer so the user doesn't have to scroll all the way up manually.
    
    Added active, active_button, return_to_top classes to CSS file

--- js/app.js
+++ js/app.js
@@ -13,71 +13,75 @@
  * 
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
 
 //Global Variables
-const navMenu = document.querySelector('.navbar__menu');
+const navMenu = document.querySelector('.navbar__menu'); 
 const navList = document.querySelector('#navbar__list');
 const allSections = document.querySelectorAll('section');
 const footer = document.querySelector('.page__footer');
+const header = document.querySelector('.page__header');
 
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
+/*
+    Start of Function declarations 
 */
 
 //Function to build the navigation bar
 const buildNaviBar = () => {
     for (const section of allSections) {
-        const newLi = document.createElement('li');
+        const newLi = document.createElement('li'); //create new list item
+        // append the new list item with the link to the section id and the name of the section.
         newLi.innerHTML = `<a href = "#${section.id}" class = "menu__link">${section.dataset.nav}`;
-        navList.appendChild(newLi);
+        navList.appendChild(newLi); //append the new list item created to the unordered list
+        scrollAction(newLi, section); //adding the smooth scrolling behavior to the section link
     }
 
-    navMenu.appendChild(navList);
+    navMenu.appendChild(navList); //append the navigation list to the navigation menu bar.
+}
+
+//Function to make sure we have a smooth scrolling behavior
+const scrollAction = (button, section) => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.scrollTo({
+            top: section.offsetTop,
+            behavior: "smooth"
+        });
+    });
 }
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-//Invoking the buildNaviBar function to build the Navigation bar.
-buildNaviBar();
-
-
-
-// Add class 'active' to section when near top of viewport
-
-//Function to designates a class "your-active-class" to the section as it becomes close to the top of the screen (viewport)
+//Function to designates a class "active" to the section as it becomes close to the top of the screen (viewport)
 const makeActive = () => {
-    for (const section of allSections) {
+    const menuLink = document.querySelectorAll(".menu__link");
+    allSections.forEach((section, i) => {
         const box = section.getBoundingClientRect(); //grabs the location of the section in relation to the viewport.
         //Find a value that works best, but 150 seems to be a good start.
         if (box.top <= 150 && box.bottom >= 150) {
         //apply active state on current section and corresponding Nav link
-            section.classList.add('your-active-class');
+            section.classList.add('active');
+            menuLink[i].classList.add('active_button');
         } else {
         //Remove active state from other section and corresponding Nav link
-            section.classList.remove('your-active-class');
+            section.classList.remove('active');
+            menuLink[i].classList.remove('active_button');
         }
-     }
+    });
 }
 
-
-// Scroll to anchor ID using scrollTO event
-
-
-
+//
+const hideNavBar = () => {
+    let scrolling;
+    //Default Settings for NavBar while scrolling
+    header.style.cssText = 'opacity: 1; transition: ease 0.2s ;'
+    // Clear timeout throughout the scrolling
+    window.clearTimeout( scrolling );
+    //The Timeout to run after scrolling ends
+    scrolling = setTimeout(() => {
+        //The Settings Executed on NavBar after Timeout finished
+        header.style.cssText = 'opacity: 0; transition: ease 0.2s ;'
+    }, 3000);
+}
 
 /**
  * End Main Functions
@@ -86,10 +90,23 @@ const makeActive = () => {
 */
 
 // Build menu 
+//Invoking the buildNaviBar function to build the Navigation bar.
+buildNaviBar();
 
-// Scroll to section on link click
+// Event listener to mark the sections as active and also hiding/appearing of  the navigation bar while scrolling 
+window.addEventListener("scroll", (event) => {
+    makeActive(); //invoke makeActive function
+    hideNavBar(); //invode hideNavBar
+});
 
-// Set sections as active
-window.addEventListener("scroll", makeActive);
+//Adding a return to top of page button at the bottom of the page with an event listener
 
-
+//Adding the HTML for the button
+const returnToTop = footer.insertAdjacentHTML('beforebegin', `<button class = "return_to_top" type= "button"> Return to Top</button>`);
+// Adding an even listener to when the button is clicked it scrolls to the top of the page
+document.querySelector(".return_to_top").addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
