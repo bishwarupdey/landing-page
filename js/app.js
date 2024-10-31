6b51cd32e48dd112b26b88d6422275f872222d7f
kooliotbone <kooliotbone@gmail.com>
2024-10-31 07:02:59 -0700

    feat
    Fixed the buildNaviBar function. It was a syntax error.
    Added a makeActive function and an EventListener to use the function.

--- js/app.js
+++ js/app.js
@@ -22,8 +22,10 @@
 //Global Variables
 const navMenu = document.querySelector('.navbar__menu');
 const navList = document.querySelector('#navbar__list');
-const allSections = document.querySelectorAll('.section');
+const allSections = document.querySelectorAll('section');
 const footer = document.querySelector('.page__footer');
+
+
 
 /**
  * End Global Variables
@@ -32,10 +34,10 @@ const footer = document.querySelector('.page__footer');
 */
 
 //Function to build the navigation bar
-function buildNaviBar() {
+const buildNaviBar = () => {
     for (const section of allSections) {
         const newLi = document.createElement('li');
-        newLi.innerText = `<a href = "#${section.id}" class = "menu__link">${section.dataset.nav}`;
+        newLi.innerHTML = `<a href = "#${section.id}" class = "menu__link">${section.dataset.nav}`;
         navList.appendChild(newLi);
     }
 
@@ -49,17 +51,32 @@ function buildNaviBar() {
  * 
 */
 
-// build the nav
-
-//Invoking the buildNaviBar 
+//Invoking the buildNaviBar function to build the Navigation bar.
 buildNaviBar();
 
 
 
 // Add class 'active' to section when near top of viewport
 
+//Function to designates a class "your-active-class" to the section as it becomes close to the top of the screen (viewport)
+const makeActive = () => {
+    for (const section of allSections) {
+        const box = section.getBoundingClientRect(); //grabs the location of the section in relation to the viewport.
+        //Find a value that works best, but 150 seems to be a good start.
+        if (box.top <= 150 && box.bottom >= 150) {
+        //apply active state on current section and corresponding Nav link
+            section.classList.add('your-active-class');
+        } else {
+        //Remove active state from other section and corresponding Nav link
+            section.classList.remove('your-active-class');
+        }
+     }
+}
+
 
 // Scroll to anchor ID using scrollTO event
+
+
 
 
 /**
@@ -73,5 +90,6 @@ buildNaviBar();
 // Scroll to section on link click
 
 // Set sections as active
+window.addEventListener("scroll", makeActive);
 
 
